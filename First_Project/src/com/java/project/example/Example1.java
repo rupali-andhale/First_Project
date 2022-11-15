9943367123a3569859ebea3062c674891cebe08c
rupali-andhale <rupaliandhale1997@gmail.com>
2022-11-15 16:18:10 +0530

    Add new class

---
+++ First_Project/src/com/java/project/example/Example1.java
@@ -0,0 +1,11 @@
+package com.java.project.example;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		System.out.println("Add new class");
+		System.out.println("Add new class");
+
+	}
+
+}
---
+++ First_Project/src/com/java/project/example/Example_2.java
@@ -0,0 +1,9 @@
+package com.java.project.example;
+
+public class Example_2 {
+
+	public static void main(String[] args) {
+		System.out.println("Myself.....");
+	}
+
+}
